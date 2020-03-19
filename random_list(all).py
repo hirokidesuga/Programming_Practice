61f6f58ec15c26a25269edc76dfcac8ee09ace6d
hirokidesuga <62142935+hirokidesuga@users.noreply.github.com>
2020-03-19 22:53:56 +0900

    ランダムリスト生成プログラム2.0(要素数、要素の最小最大値入力方式)

--- random_list(all).py
+++ random_list(all).py
@@ -36,26 +36,14 @@ def random_list(b):
 def list_max(a):
     if not a:
         return "なし"
-    max_value = a[0]
-    for item in a:
-        if item > max_value:
-            max_value = item
+    max_value = min(a)
     return max_value
-
 
 def list_min(a):
     if not a:
         return "なし"
-    min_value = a[0]
-    for item in a:
-        if item < min_value:
-            min_value = item
+    min_value = min(a)
     return min_value
-
-
-def list_len(a):
-    return len(a)
-
 
 def list_sum(a):
     val_sum = 0
@@ -68,7 +56,7 @@ def play():
     user_input = get_user_input()
     l = random_list(user_input)
     print(list(l))
-    print("要素は{}個".format(list_len(l)))
+    print("要素は{}個".format(len(l)))
     print("合計は{}".format(list_sum(l)))
     print("最大値は{}".format(list_max(l)))
     print("最小値は{}".format(list_min(l)))
